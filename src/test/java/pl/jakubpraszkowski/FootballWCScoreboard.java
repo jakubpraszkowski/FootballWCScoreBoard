d39ab702b4c61a59f6eb1416199ea5fe06e82d24
Jakub Praszkowski <kubapraszkowski@gmail.com>
2025-09-09 12:18:02 +0200

    feat: create a Football scboard class

---
+++ src/test/java/pl/jakubpraszkowski/FootballWCScoreboard.java
@@ -0,0 +1,5 @@
+package pl.jakubpraszkowski;
+
+public class FootballWCScoreboard {
+    
+}
--- src/test/java/pl/jakubpraszkowski/FootballWCScoreboardTest.java
+++ src/test/java/pl/jakubpraszkowski/FootballWCScoreboardTest.java
@@ -1,4 +1,18 @@
 package pl.jakubpraszkowski;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class FootballWCScoreboardTest {
+    private FootballWCScoreboard board;
+
+    @BeforeEach
+    void setUp() {
+        board = new FootballWCScoreboard();
+    }
+
+    @Test
+    void startGameShouldInitializeWithZeroScore() {
+
+    }
 }
